7e3301509869776a63c2877e42b74f2593746797
devu2 <daviduwayezu400@gmail.com>
2020-09-28 04:04:04 -0700

    ft-users

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,14 @@ const express = require('express');
 const router = express.Router();
 const blogRoutes = require('../routes/blogRoutes');
 const queriesRoutes = require('../routes/queriesRoutes');
+const userRoutes = require('../routes/userRoutes');
+
 
 router.use('/api/blogs',blogRoutes);
-router.use('/api/queries',queriesRoutes)
+router.use('/api/queries',queriesRoutes);
+router.use('/api/users',userRoutes);
+router.use('/api/user/signin',userRoutes);
+router.use('/api/user/update',userRoutes)
 
 
 module.exports = router;
