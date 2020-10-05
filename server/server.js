eee94b39925e9ac8c3c7a053e8e5e1cf97883882
devu2 <daviduwayezu400@gmail.com>
2020-10-05 09:43:34 +0200

    swagger Documentation

--- server/server.js
+++ server/server.js
@@ -1,25 +1,30 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const jwtStrategy = require('./config/passport');
-const passport = require('passport');
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
-
-
-
+const jwtStrategy = require("./config/passport");
+const passport = require("passport");
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const swagger = require("swagger-ui-express");
+const docs = require("../swagger.json");
 
 const app = express();
 
-mongoose.connect(process.env.NODE_ENV==='test'?process.env.MONGO_URL_TEST: process.env.MONGO_URL,{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-.then(()=>{
-    console.log('database Connected successfully!')
-})
+mongoose
+  .connect(
+    process.env.NODE_ENV === "test"
+      ? process.env.MONGO_URL_TEST
+      : process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+  )
+  .then(() => {
+    console.log("database Connected successfully!");
+  });
 app.use(passport.initialize());
-passport.use(jwtStrategy)
+passport.use(jwtStrategy);
 app.use(express.json());
 app.use(routes);
-
+app.use("/api-docs", swagger.serve, swagger.setup(docs));
 
 app.listen(5500);
 module.exports = app;
